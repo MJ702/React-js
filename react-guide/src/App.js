8146c1529a3efa7319e14c33fbccad08563d6522
MJ702 <kasodiyamit1234@gamil.com>
2020-09-19 15:32:48 +0530

    add a new Person componente

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>I'm React developer </h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>I'm React developer </h1>
+            <p>This is really working!</p>
+            <Person />
+        </div>
+    );
 }
 
 export default App;
