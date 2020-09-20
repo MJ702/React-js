1fff48921e6a4b43c7a0311fe6430336248ec338
MJ702 <kasodiyamit1234@gamil.com>
2020-09-20 10:34:42 +0530

    state

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>I'm React developer </h1>
-            <p>This is really working!</p>
-            <Person name={'Meet'} age={26}/>
-            <Person name={'Radha'} age={29}> My hobbies : love Krisha </Person>
-            <Person name={'Mit'} age={19}/>
+class App extends Component {
+    state = {
+        persons : [
+            { name: 'MEET', age: 25},
+            { name: 'AMISHA', age: 23},
+            { name: 'RADHA', age: 34}
+        ]
+    }
 
-        </div>
-    );
+
+    render() {
+        return (
+            <div className="App">
+                <h1>I'm React developer </h1>
+                <p>This is really working!</p>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies : love Krisha </Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
