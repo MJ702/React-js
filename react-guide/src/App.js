26d6a3a13eddfa1159a61391095472866b3fa589
MJ702 <kasodiyamit1234@gamil.com>
2020-09-20 09:55:11 +0530

    props

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -7,7 +7,10 @@ function App() {
         <div className="App">
             <h1>I'm React developer </h1>
             <p>This is really working!</p>
-            <Person />
+            <Person name={'Meet'} age={26}/>
+            <Person name={'Radha'} age={29}> My hobbies : love Krisha </Person>
+            <Person name={'Mit'} age={19}/>
+
         </div>
     );
 }
