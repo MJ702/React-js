d8eccfab6c2520fe543f2e6891f3bccc7e0cff0e
MJ702 <kasodiyamit1234@gamil.com>
2020-09-20 11:06:20 +0530

    add SwitchNameHandler

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -6,18 +6,27 @@ class App extends Component {
     state = {
         persons : [
             { name: 'MEET', age: 25},
-            { name: 'AMISHA', age: 23},
+            { name: 'Krishna', age: 23},
             { name: 'RADHA', age: 34}
         ]
     }
 
+    switchNameHandler = () => {
+        this.setState({
+            persons : [
+                { name: 'Meetkasodiya', age:25},
+                { name: 'Radha', age: 14},
+                { name: 'Krishna', age: 28}
+            ]
+        })
+    }
 
     render() {
         return (
             <div className="App">
                 <h1>I'm React developer </h1>
                 <p>This is really working!</p>
-                <button>Switch Name</button>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies : love Krisha </Person>
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
