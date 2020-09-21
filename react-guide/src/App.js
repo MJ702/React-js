1278fe803684bc0ae061b5685846b313b1b3b831
MJ702 <kasodiyamit1234@gamil.com>
2020-09-21 13:36:42 +0530

    use react hook

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,38 +1,36 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-    state = {
-        persons : [
-            { name: 'MEET', age: 25},
-            { name: 'Krishna', age: 23},
-            { name: 'RADHA', age: 34}
-        ]
-    }
-
-    switchNameHandler = () => {
-        this.setState({
+const App = props => {
+        const [personsState, setPersonState ] = useState({
             persons : [
-                { name: 'Meetkasodiya', age:25},
-                { name: 'Radha', age: 14},
-                { name: 'Krishna', age: 28}
+                { name: 'MEET', age: 25},
+                { name: 'Krishna', age: 23},
+                { name: 'RADHA', age: 34}
             ]
         })
-    }
 
-    render() {
+        const switchNameHandler = () => {
+            setPersonState({
+                persons : [
+                    { name: 'Meetkasodiya', age:25},
+                    { name: 'Radha', age: 14},
+                    { name: 'Krishna', age: 28}
+                ]
+            })
+        }
         return (
             <div className="App">
                 <h1>I'm React developer </h1>
                 <p>This is really working!</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies : love Krisha </Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <button onClick={switchNameHandler}>Switch Name</button>
+                <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+                <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> My hobbies : love Krisha </Person>
+                <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
             </div>
         );
-    }
+
 
 
 }
